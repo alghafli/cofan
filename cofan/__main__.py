3569ddec967a4aeb5b9390c187d50a387c044c9c
alghafli <37610446+alghafli@users.noreply.github.com>
2022-07-09 09:13:08 +0300

    Add files via upload

--- cofan/__main__.py
+++ cofan/__main__.py
@@ -1,12 +1,20 @@
+import pathlib
 from .cofan import *
+import re
 import sys
 import argparse
+from .utils.misc import tree_patterner
+from .utils.tfman import TFMan
 
 parser = argparse.ArgumentParser(description='Serve files over http.')
 parser.add_argument(
-        '-a', '--addr', type=str, action='store', default='localhost:8000',
-        help='http bind address in the form <ip>:<port>'
-    )
+  '-a', '--addr', type=str, action='store', default='localhost:8000',
+  help='http bind address in the form <ip>:<port>'
+)
+parser.add_argument(
+  '-u', '--upload_password', action='store', default='',
+  help='upload password. defaults to empty string which disables uploads'
+)
 parser.add_argument(
         'root', action='store', nargs='?', default='.',
         help='root directory to serve through http'
@@ -18,18 +26,20 @@ args = parser.parse_args()
 addr = args.addr.split(':')
 addr[1] = int(addr[1])
 root = args.root
+upload_password = args.upload_password
 
-provider = Patterner()
-icons = Iconer()
-if pathlib.Path(root).is_dir():
-    browser = Filer(root, iconer=icons)
-else:
-    browser = Ziper(root, iconer=icons)
+main_tree = pathlib.Path(__file__).parent / 'main_tree'
+env = {'root': root}
+env['upload_password'] = upload_password
+env['tfman'] = TFMan
 
-provider.add('__icons__/', icons)
-provider.add('', browser)
+localhost_checker = IPPatterner()
+localhost_checker.add(re.compile(r'localhost|127.0.0.1'),
+  tree_patterner(main_tree / 'localhost_tree', env))
+localhost_checker.add(re.compile(r'.*'),
+  tree_patterner(main_tree / 'others_tree', env))
 
-handler = BaseHandler(provider)
+handler = BaseHandler(localhost_checker)
 
 srv = Server(tuple(addr), handler)
 print('serving {} at {}:{}'.format(pathlib.Path(root).resolve(), *addr))
@@ -55,3 +65,4 @@ try:
     srv.serve_forever()
 except KeyboardInterrupt:
     pass
+
